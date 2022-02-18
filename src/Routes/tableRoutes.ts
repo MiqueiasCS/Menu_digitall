f5071922bb98a60c8a8dcfb41b2963f1dc25d9ca
Miqueias <miqs_carvalho@hotmail.com>
2022-02-18 14:47:57 -0300

    feat:adding authentication in table creation route

--- src/Routes/tableRoutes.ts
+++ src/Routes/tableRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { GetTableBills } from "../Controllers/billController";
+import { authentication } from "../Middlewares/authentication";
 import {
   createTable,
   getAllTable,
@@ -9,7 +10,7 @@ import {
 const router = Router();
 
 export const tableRouter = () => {
-  router.post("", createTable);
+  router.post("", authentication, createTable);
   router.get("/:tableId/bills", GetTableBills);
   router.get("", getAllTable);
   router.get("/:tableidentifier", getTableByName);
