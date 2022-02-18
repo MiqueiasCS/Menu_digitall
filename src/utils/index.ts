12d027ee0aea955c25c0b8dfed64455d12981ec3
Miqueias <miqs_carvalho@hotmail.com>
2022-02-18 15:12:08 -0300

    fix: refactoring billService

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,9 @@
 import { getRepository } from "typeorm";
 import { OrderProduct } from "../Entities/orderProductEntites";
 import { OrderDispatched } from "../Entities/orderDispatched";
+import { Bill } from "../Entities/billEntities";
+import { Order } from "../Entities/orderEntites";
+import { AppError } from "../Errors";
 
 export const getOrderProductList = async (list: any) => {
   const orderProductRepository = getRepository(OrderProduct);
@@ -81,4 +84,95 @@ export const listDispactchedOrdersByTable = async (tableOrderList: any) => {
   }
 
   return orderDispatchListResponse;
+};
+
+export const formattedOrders = (orderProductlist: any) => {
+  let orderProductList = [];
+  for (let i = 0; i < orderProductlist.length; i++) {
+    let productResponse = {
+      productId: orderProductlist[i].product.id,
+      product: orderProductlist[i].product.name,
+      price: orderProductlist[i].product.price,
+      quantity: orderProductlist[i].product_quantity,
+    };
+    orderProductList.push(productResponse);
+  }
+  return orderProductList;
+};
+
+export const findBill = async (order: any, getASingleBill: boolean = false) => {
+  const billRepository = getRepository(Bill);
+  const ordersProductRepository = getRepository(OrderProduct);
+
+  let bill = await billRepository.findOne({
+    where: { order },
+    relations: ["order"],
+  });
+
+  if (!bill) {
+    if (getASingleBill) {
+      throw new AppError("There is no bill for this table", 400);
+    } else {
+      return {
+        orderID: order.id,
+        message: "There is no bill registered for this order",
+      };
+    }
+  }
+
+  let orderProductlist = await ordersProductRepository.find({
+    where: { order },
+    relations: ["product"],
+    select: ["product", "product_quantity"],
+  });
+
+  const billResponse = {
+    billId: bill.id,
+    date: bill.bill_date,
+    orderId: order.id,
+    client: order.client,
+    finalPrice: bill.final_price,
+    formOfPayment: bill.form_of_payment,
+    orders: formattedOrders(orderProductlist),
+  };
+
+  return billResponse;
+};
+
+export const listBills = async (orderList: any) => {
+  let billsList = [];
+  for (let index = 0; index < orderList.length; index++) {
+    let bill = await findBill(orderList[index]);
+    billsList.push(bill);
+  }
+  return billsList;
+};
+
+export const getOrder = async (orderId: string) => {
+  const orderRepository = getRepository(Order);
+
+  let order = await orderRepository.findOne({
+    where: { id: orderId },
+    relations: ["table", "dispatched", "order_product"],
+  });
+
+  if (!order) {
+    throw new AppError("The Order does not exist", 404);
+  }
+
+  return order;
+};
+
+export const getFinalPrice = async (order: any) => {
+  const ordersProductRepository = getRepository(OrderProduct);
+  const orderProductlist = await ordersProductRepository.find({
+    where: { order },
+    relations: ["product"],
+  });
+
+  let finalPrice = orderProductlist.reduce((ac, item) => {
+    return ac + item.product.price * item.product_quantity;
+  }, 0);
+
+  return finalPrice;
 };
