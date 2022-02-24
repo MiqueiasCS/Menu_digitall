11f92771c25a991e2f34a2b0796ef12eeaff37ce
Arthur <arthur.linemburg@unesc.net>
2022-02-24 12:49:45 -0300

    feat: managment control

--- src/Routes/userRoutes.ts
+++ src/Routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-import { createUser, login } from '../Controllers/userController'
+import { createUser, getDailyBalance, login } from '../Controllers/userController'
+import { authentication } from '../Middlewares/authentication'
 import { validateDataSchema } from '../Middlewares/validateDataSchema'
 import { loginSchema } from '../Schemas/loginSchema'
 import { userSchema } from '../Schemas/userSchema'
@@ -9,6 +10,7 @@ const router = Router()
 export const userRouter = () => {
   router.post('', validateDataSchema(userSchema), createUser)
   router.post('/login', validateDataSchema(loginSchema), login)
+  router.get('/daily_balance', authentication, getDailyBalance)
   
   return router
 }
